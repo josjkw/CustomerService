55dafcd408f190b5fe3bd87f14b499060dba4dc1
jo <josselin.druard@metaco.com>
2024-04-03 14:35:16 +0200

    => F[A] to F[A]

--- customer-service/src/main/scala/infrastructure/helpers/retryhelpers/RetryHelpers.scala
+++ customer-service/src/main/scala/infrastructure/helpers/retryhelpers/RetryHelpers.scala
@@ -7,7 +7,7 @@ import scala.concurrent.duration.Duration
 
 object RetryHelpers {
 
-  def retryOnAllErrors[A, F[_]: Async](action: => F[A], delay: Duration, retries: Int): F[A] = {
+  def retryOnAllErrors[A, F[_]: Async](action: F[A], delay: Duration, retries: Int): F[A] = {
     action.attempt.flatMap {
       case Left(_) if retries > 0 => Async[F].sleep(delay) *> retryOnAllErrors(action, delay, retries - 1)
       case Left(e)                => Async[F].raiseError(e)
